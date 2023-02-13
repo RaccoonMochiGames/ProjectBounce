25153a442887ff9afc0901ffcba158f2f99a84c1
RenDeveloper <duncansrendall@gmail.com>
2023-02-13 19:56:45 +0000

    refreshed gitignore

--- Intermediate/Build/Win64/ProjectBounceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30145
-#include "M:/Unreal/PROFPROJECT/ProjectBounce/Intermediate/Build/Win64/ProjectBounceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- Intermediate/Build/Win64/UnrealEditor/Inc/ProjectBounce/ProjectBounceCharacter.generated.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:91a19922e0ca5549dedc2fc5d4444540d19ad388a0254c48784d5c648de6b7fb
-size 4762
