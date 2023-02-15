5b76b545ac96c57b75831a385756c6cf9e4c0300
TADAS RAUDONIS <2003256@uad.ac.uk>
2023-02-15 16:18:13 +0000

    adjusted projectile params
    -projectile is smaller
    -projectiles friction and bounciness adjusted
    -projectile no longer destroys itself

--- Source/ProjectBounce/ProjectBounceProjectile.cpp
+++ Source/ProjectBounce/ProjectBounceProjectile.cpp
@@ -28,7 +28,7 @@ AProjectBounceProjectile::AProjectBounceProjectile()
 	ProjectileMovement->bShouldBounce = true;
 
 	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	InitialLifeSpan = 0.0f;
 }
 
 void AProjectBounceProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
@@ -37,7 +37,5 @@ void AProjectBounceProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* Other
 	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
 	{
 		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
-
-		Destroy();
 	}
 }
