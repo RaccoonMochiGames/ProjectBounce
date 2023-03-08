861aa40ee751352eba91390643f2e01e696fc431
TADAS RAUDONIS <2003256@uad.ac.uk>
2023-03-08 14:59:45 +0000

    super wonky player taking damage

--- Source/ProjectBounce/ProjectBounceCharacter.h
+++ Source/ProjectBounce/ProjectBounceCharacter.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49dfdc2ca5e079f4392bf4cad28cab9b1bb8b7644fb8103ae0dc860298891248
-size 4292
+oid sha256:0e7bd9c546bfcf20624c667f8288a96ac4857b8e4d1036a1277a50b8fb9c9aa3
+size 4801
